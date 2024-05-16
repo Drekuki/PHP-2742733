0eae29885c2ffc4211243774a2aeca2d9c615c91
Tomás Garrido <tgarrido3225@gmail.com>
2024-05-16 15:00:16 -0500

    feat: ejercicio-01 y ejercicio-02

---
+++ 04-get-post/ejercicio-01.php
@@ -0,0 +1,31 @@
+<?php
+
+$Edad = $_GET['Edad'];
+
+if($Edad <= 17) {
+    echo "Eres menor de edad";
+} else {
+    echo "Eres mayor de edad";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="ejercicio-01.php" method="$_GET">
+        <br>
+        <label for="Edad"></label>
+        <input id="Edad" type="text" placeholder="Edad del usuario..." name="Edad">
+        <br> <br>
+        <button type="submit">Enviar</button>
+    </form>
+
+</body>
+</html>
