5c84f4075f1306e3197a2fc16fdcb4050f2c5c7f
Tomás Garrido <tgarrido3225@gmail.com>
2024-05-20 16:00:24 -0500

    Feat: Cambios

---
+++ 06-funtions-php/index.php
@@ -0,0 +1,33 @@
+<?php
+
+echo "ejecutando php";
+
+echo "<br/>";
+
+function validarEdad ($edad) {
+    if ($edad >= 18) {
+        echo "mayor de edad";
+    } else {
+        echo " <br/>  menor de edad";
+    }
+}
+
+//validar funcion
+
+validarEdad (12);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-color:black; color:white">
+    
+    <h1>Funciones PHP 🐱‍🏍🤳</h1>
+
+</body>
+</html>
